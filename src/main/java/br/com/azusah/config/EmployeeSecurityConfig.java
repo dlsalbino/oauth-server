b02fd3cf37928a2753cb551e0d8a3efe0499dd3a
Daniel Albino <daniel.albino@gmail.com>
2020-05-25 23:39:28 -0300

    Creating WebSecurity config class.

---
+++ src/main/java/br/com/azusah/config/EmployeeSecurityConfig.java
@@ -0,0 +1,35 @@
+package br.com.azusah.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class EmployeeSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").permitAll()
+                .antMatchers("/users/employees").hasAnyRole("ADMIN").anyRequest().authenticated()
+                .and().formLogin().permitAll()
+                .and().logout().permitAll();
+        http.csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("admin")
+                .authorities("ROLE_ADMIN");
+    }
+}
